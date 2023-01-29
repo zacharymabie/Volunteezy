05f44b8ea0c5a6a79fb32bf6b24c0dfc81ef3180
Brian <80086776+brianeide@users.noreply.github.com>
2023-01-29 13:24:44 -0600

    fix

--- backend/util.js
+++ backend/util.js
@@ -1,6 +1,7 @@
 const { User } = require("./models/user.js");
 
 async function authenticate(token, userID) {
+    if (!userID) return false;
     if (userID.length !== 24) return false;
     const user = await User.findById(userID);
     if (!user) return false;
